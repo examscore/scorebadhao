32c2651298788a9affd0f5e1e99d0bb066056169
examscore <scorebadhao@gmail.com>
2026-06-30 09:19:38 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@
  */
 
 const SCOREBADHAO_CONFIG = {
-  API_URL: "https://docs.google.com/spreadsheets/d/17RfyvLRzJhULIzmsWPgBC82-DgxLd83BqTfPBzDhZzM/edit?usp=drivesdk",
+  API_URL: "https://script.google.com/macros/s/AKfycbzx9VKm6i3OQPBEs1LwtuMVhk5jKVZ5aSG0yyRydUolDmQK5uqTNZqPxB0F2nzCqlJO/exec",
   APP_NAME: "ScoreBadhao",
   VERSION: "1.2",
   CACHE_TTL_MS: 5 * 60 * 1000
