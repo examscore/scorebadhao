b2b8e13dd5e9dc071eb2c9e8ae580eba883664b5
examscore <scorebadhao@gmail.com>
2026-06-30 09:49:54 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,7 +6,7 @@
  */
 
 const SCOREBADHAO_CONFIG = {
-  API_URL: "PASTE_DEPLOYED_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbzx9VKm6i3OQPBEs1LwtuMVhk5jKVZ5aSG0yyRydUolDmQK5uqTNZqPxB0F2nzCqlJO/exec",
   APP_NAME: "ScoreBadhao",
   VERSION: "1.2",
   CACHE_TTL_MS: 5 * 60 * 1000
